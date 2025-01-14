9ba29a96446a9036fa1f82bd64bc9113c2df46e0
冰梦 <1580777927@qq.com>
2025-01-14 20:53:42 +0800

    build(dependencies): 更新依赖并添加 Pug 渲染器
    - 将 hexo-renderer-stylus 升级到 3.0.1
    - 添加 hexo-renderer-pug 作为新的依赖项
    - 更新 package-lock.json 中的多个依赖项版本
    - 添加外挂标签

---
+++ themes/butterfly/scripts/tag/link.js
@@ -0,0 +1,32 @@
+const urlFor = require("hexo-util").url_for.bind(hexo);
+function link(args) {
+  args = args.join(" ").split(",");
+  let title = args[0];
+  let text = args[1];
+  let link = args[2];
+  let imgUrl = args[3] || "";
+  let InsideStation = false;
+  link = link.trim();
+  imgUrl = imgUrl.trim();
+  // 获取网页favicon
+  if (!imgUrl) {
+    let urlNoProtocol = link.replace(/^https?\:\/\//i, "");
+    // imgUrl = "https://api.iowen.cn/favicon/" + urlNoProtocol + ".png";
+    imgUrl = "https://ico.kucat.cn/get.php?url=" + urlNoProtocol;
+  }
+  if (imgUrl == "true") {
+    InsideStation = true;
+  }
+  return `<div class='tag link'><a class="link-card" target="_blank" href="${urlFor(link)}">
+    <div class="tips">${InsideStation ? "站内地址" : "引用站外地址"}</div>
+    <div class="link-group">
+        <div class="left" style="background-image: url(${InsideStation ? "/img/siteicon/android-chrome-512x512.png" : imgUrl});"></div>
+        <div class="right">
+            <div class="title">${title}</div>
+            <div class="text">${text}</div>
+        </div>
+        <i class="fas fa-angle-right"></i>
+    </div>
+    </a></div>`;
+}
+hexo.extend.tag.register("link", link, { ends: false });
